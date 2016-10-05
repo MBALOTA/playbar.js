3cbcc55f5c1ba496a86cedf04aa98dd6d0bf55b5
Matthias Balota <balotamatthias@gmail.com>
2016-10-05 16:27:05 +0200

    Refactored Code, Added Loaded Progress
    Loaded Progress behaves somewhat like the normal Progress. For more info
    read the comments in playbar.js

--- README.md
+++ README.md
@@ -4,25 +4,31 @@ A small and simple playbar written in pure js. Check the example [website] (http
 HOW TO USE:
 
 1. script source the minified playbar.js (playbar.min.js)
-2. initialize a custom playBar:
+2. initialize a custom playBar, e.g.:
 ```js
   var newPlayBar = new playBar ("#wrapperId", {
-                                                buttonId: "#bid",     //default: "#playBarButton"
-                                                progressId: "#pid",   //default: "#playBarProgress"
-                                                previewId: "#ppid",   //default: "#playBarProgressPreview"
+                                                buttonId: "#bid",     //default: "#pb-Button"
+                                                progressId: "#pid",   //default: "#pb-Progress"
+                                                previewId: "#ppid",   //default: "#pb-Progress-Preview"
+                                                loadedId: "#plid",   //default: "#pb-Progress-Loaded"
+                                                moveButtonToPreview: true, //if 'true', button will move to preview progress
                                                 enableButtonOffset: true, //to enable out of range button offset, incl. custom offset
                                                 buttonOffset: {5,0},  //x,y IN PX
                                                 barType: "horizontal",//can be "vertical"
-                                                showPreview: true
+                                                showPreview: true, //enables previewProgress
+                                                showLoaded: true //enables loadedProgress
                                                 });
 ```
 Don't worry, a lot of the options are "optional" *badum tsss*.
 
-There are 2 functions:
+FOR MORE DOCUMENTATION READ THE DEV-COMMENTS IN THE NORMAL playbar.js!
+
+There are 3 functions:
 ```js
 playBar.setProgress(number); //0-100, progressId.style.width/height & buttonId.style.left/bottom will be set to px
-playBar.setPreviewProgress(number); /* 0-100, the button will update its position,
+playBar.setPreviewProgress(number); /* 0-100, the button will update its position, if moveButtonToPreview is set to true ,
                                               if the preview progress option is enabled and if the preview progress isn't 0 */
+playBar.setLoadedProgress(number); // 0-100, it is like the normal progress
 ```
 
 For more information check out the pretty version! playbar.js
--- js/playbar.js
+++ js/playbar.js
@@ -1,3 +1,13 @@
+//------------------------   CREDITS   ------------------------//
+/*
+  first contributor:
+  - MATTHIAS BALOTA
+
+  THIS PROJECT USES THE UNLICENSE, have fun!
+*/
+//------------------------ CREDITS END ------------------------//
+
+//------------------------ HERE IS WHERE THE MAGIC HAPPENS ------------------------//
 function getPosition(el) {
   var xPos = 0;
   var yPos = 0;
@@ -35,28 +45,32 @@ function updateDOM (playBar) {
   var buttonEl = document.querySelector(playBar.buttonId);
   var progressEl = document.querySelector(playBar.progressId);
   var previewEl;
+  var loadedEl;
 
-  if(playBar.showPreview) {
-    previewEl = document.querySelector(playBar.previewId);
-  };
+  if(playBar.showPreview) previewEl = document.querySelector(playBar.previewId);
+  if(playBar.showLoaded) loadedEl = document.querySelector(playBar.loadedId);
 
   var buttonProgress = playBar.progress;
-  if(playBar.showPreview&&playBar.previewProgress!==0) buttonProgress = playBar.previewProgress;
+  if(playBar.showPreview && playBar.previewProgress!==0 && playBar.moveButtonToPreview) buttonProgress = playBar.previewProgress;
 
   if(playBar.barType==="horizontal")
   {
+    var wDivided = wrapperEl.offsetWidth / 100;
+
     var buttonOffset = playBar.buttonOffset.x;
-    var progress = wrapperEl.offsetWidth / 100 * playBar.progress;
-    var previewProgress = wrapperEl.offsetWidth / 100 * playBar.previewProgress;
+    var progress = wDivided * playBar.progress;
+    var previewProgress = wDivided * playBar.previewProgress;
+    var loadedProgress = wDivided * playBar.loadedProgress;
     var progressMAX = wrapperEl.offsetWidth;
     var buttonMAX = wrapperEl.offsetWidth - buttonEl.offsetWidth;
     var buttonMIN = 0;
 
-    var buttonLeftInPX = (wrapperEl.offsetWidth / 100 * buttonProgress) ;
+    var buttonLeftInPX = (wDivided * buttonProgress) ;
     if(playBar.enableButtonOffset) {
       buttonLeftInPX = buttonLeftInPX + ( -buttonEl.offsetWidth - buttonOffset);
       buttonMIN += buttonOffset;
       buttonMAX += buttonOffset + buttonEl.offsetWidth;
+      //buttonEl.style.bottom = playBar.buttonOffset.y+"px";
     } else {
       buttonLeftInPX -= buttonEl.offsetWidth/2
     };
@@ -66,21 +80,25 @@ function updateDOM (playBar) {
 
     if(playBar.previewProgress===0) previewProgress=0;
     if(playBar.showPreview) previewEl.style.width = previewProgress.clamp(0, progressMAX)+"px";
+    if(playBar.showLoaded) loadedEl.style.width = loadedProgress.clamp(0, progressMAX)+"px";
   }
   else if (playBar.barType==="vertical")
   {
+    var hDivided = wrapperEl.offsetHeight / 100;
+
     var buttonOffset = playBar.buttonOffset.y;
-    var progress = wrapperEl.offsetHeight / 100 * playBar.progress;
-    var previewProgress = wrapperEl.offsetHeight / 100 * playBar.previewProgress;
+    var progress = hDivided * playBar.progress;
+    var previewProgress = hDivided * playBar.previewProgress;
     var progressMAX = wrapperEl.offsetHeight;
     var buttonMAX = wrapperEl.offsetHeight - buttonEl.offsetHeight;
     var buttonMIN = 0;
 
-    var buttonBottomInPX = (wrapperEl.offsetHeight / 100 * buttonProgress);
+    var buttonBottomInPX = (hDivided * buttonProgress);
     if(playBar.enableButtonOffset) {
       buttonBottomInPX = buttonBottomInPX + ( -buttonEl.offsetHeight - buttonOffset);
       buttonMIN += buttonOffset;
       buttonMAX += buttonOffset + buttonEl.offsetWidth;
+      //buttonEl.style.left = playBar.buttonOffset.x+"px";
     } else {
       buttonBottomInPX -= buttonEl.offsetHeight/2
     };
@@ -90,40 +108,55 @@ function updateDOM (playBar) {
 
     if(playBar.previewProgress===0) previewProgress=0;
     if(playBar.showPreview) previewEl.style.height = previewProgress.clamp(0, progressMAX)+"px";
+    if(playBar.showLoaded) loadedEl.style.height = loadedProgress.clamp(0, progressMAX)+"px";
   };
 };
+//------------------------ MAGIC END ------------------------//
 
+//------------------------ PLAYBAR ------------------------//
 var playBar = function (id, options) {
   var pb = {
-    id: id, //e.g. #playBarWrapper
-    buttonId: "#playBarButton",
-    progressId: "#playBarProgress",
-    previewId: "#playBarProgressPreview",
+    id: id, //e.g. '#pb-Wrapper'
+    buttonId: "#pb-Button",
+    progressId: "#pb-Progress",
+    previewId: "#pb-ProgressPreview",
+    loadedId: "#pb-ProgressLoaded",
+    moveButtonToPreview: true, //if 'true', button will move to preview progress
+    enableButtonOffset: false, //buttonOffset will only work if this is set to 'true'
     buttonOffset: {
       x: 0, //e.g. 6 OR -10 IN PX
       y: 0
     },
-    enableButtonOffset: false,
-    barType: "horizontal", //horizontal or vertical
+    barType: "horizontal", //horizontal or vertical ELSE IT WON'T WORK!
     progress: 0, //in %
     previewProgress: 0, //in %,
-    showPreview: false,
+    loadedProgress: 0, //in %
+    showPreview: false, //if 'true', this will show the previewProgress
+    showLoaded: false, //if 'true', this will show the loadedProgress
     setProgress: function(newProgressPercent) {
-      if(typeof newProgressPercent === "number") {
-        this.progress = newProgressPercent;
-        updateDOM(this);
-      }
+      if(typeof newProgressPercent !== "number") return;
+      this.progress = newProgressPercent;
+      updateDOM(this);
     },
     setPreviewProgress: function(newProgressPercent) {
-      if(typeof newProgressPercent === "number") this.previewProgress = newProgressPercent;
+      if(typeof newProgressPercent !== "number") return;
+      this.previewProgress = newProgressPercent;
+      updateDOM(this);
+    },
+    setLoadedProgress: function(newLoadedPercent) {
+      if(typeof newLoadedPercent !== "number") return;
+      this.loadedProgress = newLoadedPercent;
       updateDOM(this);
     }
   };
 
+  //------------------------ Update PlayBar to custom Options ------------------------//
   if(typeof options.buttonId !== "undefined" && typeof options.buttonId === "string") pb.buttonId = options.buttonId;
   if(typeof options.progressId !== "undefined" && typeof options.progressId === "string") pb.progressId = options.progressId;
   if(typeof options.previewId !== "undefined" && typeof options.previewId === "string") pb.previewId = options.previewId;
+  if(typeof options.loadedId !== "undefined" && typeof options.loadedId === "string") pb.loadedId = options.loadedId;
 
+  if(typeof options.moveButtonToPreview !== "undefined" && typeof options.moveButtonToPreview === "boolean") pb.moveButtonToPreview = options.moveButtonToPreview;
   if(typeof options.enableButtonOffset !== "undefined" && typeof options.enableButtonOffset === "boolean") pb.enableButtonOffset = options.enableButtonOffset;
   if(typeof options.buttonOffset !== "undefined") pb.buttonOffset = options.buttonOffset;
   if(typeof options.barType !== "undefined") {
@@ -134,9 +167,14 @@ var playBar = function (id, options) {
   };
 
   if(typeof options.showPreview !== "undefined" && typeof options.showPreview === "boolean") pb.showPreview = options.showPreview;
+  if(typeof options.showLoaded !== "undefined" && typeof options.showLoaded === "boolean") pb.showLoaded = options.showLoaded;
   return pb;
 };
+//------------------------  PLAYBAR END  ------------------------//
 
-// exports.playBar = function(id, options) {
-//   return playBar(id,options);
-// }
+/*
+//Used for nodejs in a small project
+exports.playBar = function(id, options) {
+  return playBar(id,options);
+}
+*/
